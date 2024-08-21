692b89c1f0f730fc980cc1f29886e98a3e037d74
silvrfox153 <matt.tierney153@gmail.com>
2024-08-21 11:35:18 -0400

    installed mongodb and mongoose

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-// https://www.traversymedia.com/products/next-js-from-scratch-2024/categories/2154793741/posts/2175322867
+// https://www.traversymedia.com/products/next-js-from-scratch-2024/categories/2154793781/posts/2175322875 == MongoDB - 6:67
 
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
