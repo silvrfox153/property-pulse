74e1e67fa05afd2e911aa744853aefc27bc898d0
silvrfox153 <matt.tierney153@gmail.com>
2024-08-29 10:51:55 -0400

    Created google provider login code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,9 @@
-// https://www.traversymedia.com/products/next-js-from-scratch-2024/categories/2154793741/posts/2175322716
+// https://www.traversymedia.com/products/next-js-from-scratch-2024/categories/2154793795/posts/2175322947
 
-import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import '@/assets/styles/globals.css';
 
 export const metadata = {
   title: 'Property Pulse',
@@ -12,13 +13,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-// https://www.traversymedia.com/products/next-js-from-scratch-2024/categories/2154793781/posts/2175322899
+// https://www.traversymedia.com/products/next-js-from-scratch-2024/categories/2154793795/posts/2175322958
 
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
